0478b3851f61ace873910ab3ab278e196e6c0be4
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-25 18:48:41 +0100

    type assertion and cleaning

--- 11_useState_Type_Assertion/src/components/state/User.tsx
+++ 11_useState_Type_Assertion/src/components/state/User.tsx
@@ -4,10 +4,14 @@ import { useState } from 'react';
 type AuthUser = {
     name: string
     email: string
-}
+};
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser>({} as AuthUser)
+    // const [user, setUser] = useState<AuthUser | null>(null); 
+                                                                // type assertion
+    const [user, setUser] = useState<AuthUser>({} as AuthUser); // if you are sur that user will be given jsut after connexion
+
+    const handleLogout = () => setUser({name: "", email: ""});
 
     const handleLogin = () => {
         setUser({
@@ -21,8 +25,7 @@ export const User = () => {
             <h2> User name is {user?.name}</h2>
             <h2> User email is {user?.email} </h2>
             <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
-
-// export default LoggedIn;
