bd30b193d1fec93b8c9738aae61ec08a4ce79e74
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-26 08:43:58 +0100

    useReducer action strict type and merging ++ types

---
+++ 13_useReducer_Strict_Action_Types/src/components/state/Counter.tsx
@@ -0,0 +1,44 @@
+import { useReducer } from 'react';
+
+type CounterState = {
+    count: number
+};
+
+type UpdateAction =  {
+    type: 'increment' | 'decrement',
+    payload: number
+};
+
+type ResetAction = {
+    type: 'reset'
+};
+
+type CounterAction = UpdateAction | ResetAction;
+
+const initialState =  { count : 0 };
+
+function reducer(state: CounterState, action: CounterAction) {
+    switch(action.type) {
+        case 'increment': 
+            return { count: state.count + action.payload };
+        case 'reset': 
+            return initialState;
+        case 'decrement': 
+            return { count: state.count - action.payload };
+        default:
+            return state;
+    }
+}
+
+export const Counter = () => {
+    const [ state, dispatch] = useReducer(reducer, initialState);
+    return (
+        <>
+            <h1>Count: {state.count}</h1>
+            <br />
+            <button onClick={ () => dispatch({ type: 'increment', payload: 10})}> Increment by 10</button> &nbsp;
+            <button onClick={ () => dispatch({ type: 'reset'})}> Reset</button> &nbsp;
+            <button onClick={ () => dispatch({ type: 'decrement', payload: 10})}> Decrement by 10</button>
+        </>
+    );
+}
