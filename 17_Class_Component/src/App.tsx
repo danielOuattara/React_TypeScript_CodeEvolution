371c7fe58e3f5abc0c7bc7b0b57adcdaf7f9fbe5
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:15:27 +0200

    class components, revisited. See  the first parts

---
+++ 17_Class_Component/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counter from "./components/class/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter message={"Hello Daniel"} />
+    </div>
+  );
+}
+
+export default App;
