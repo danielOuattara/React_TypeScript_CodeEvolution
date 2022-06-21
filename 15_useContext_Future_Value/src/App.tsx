c66361332c827a6027253a585aaa9f34800ff8be
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:12:44 +0200

    useContext hook: future value

---
+++ 15_useContext_Future_Value/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Box } from "./components/context/Box";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { User } from "./components/context/User";
+import { UserContextProvider } from "./components/context/UserContext";
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <br />
+      <UserContextProvider>
+        <User />
+      </UserContextProvider>
+    </div>
+  );
+}
+
+export default App;
