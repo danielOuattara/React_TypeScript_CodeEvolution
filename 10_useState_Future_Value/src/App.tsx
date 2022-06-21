af8d1150d8ca3de74587af2cb0a68751b76e8e11
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:10:29 +0200

    useState hook declaration: future  value

--- 10_useState_Future_Value/src/App.tsx
+++ 10_useState_Future_Value/src/App.tsx
@@ -1,19 +1,14 @@
-import './App.css';
-import { LoggedIn } from './components/state/LoggedIn';
-import { User } from './components/state/User';
-
+import "./App.css";
+import { LoggedIn } from "./components/state/LoggedIn";
+import { User } from "./components/state/User";
 
 function App() {
-
-
   return (
     <div className="App">
       <LoggedIn />
-
       <hr />
-      <User/>
+      <User />
     </div>
-
   );
 }
 
