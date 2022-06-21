a29b5a1c4ea6e82607913eabeb180f98ffdd07cf
danielOuattara <daniel.ouattara@gmx.com>
2022-06-21 23:13:42 +0200

    useRef hook:  dom ref & mutable ref

---
+++ 16_useRef_Hook/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import DomRef from "./components/ref/DomRef";
+import { MutableRef } from "./components/ref/MutableRef";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter message="Hello Daniel/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 16_useRef_Hook/src/components/ref/DomRef.tsx
@@ -0,0 +1,13 @@
+import { useRef, useEffect } from "react";
+
+const DomRef = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  return <input type="text" ref={inputRef} />;
+};
+
+export default DomRef;
