d46e79d98cba9cf7470df24480d2b2ffece10219
danielOuattara <daniel.ouattara@gmx.com>
2023-02-23 23:51:59 +0100

    logic update for both func & class comps

--- 09_useState_Hooks/src/_01_functional/AppFunction.tsx
+++ 09_useState_Hooks/src/_01_functional/AppFunction.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import "./../App.css";
 import { LoggedIn } from "./components/state/LoggedIn";
 
 function App() {
   return (
     <div className="App">
+      <h2>Functional Component</h2>
       <LoggedIn />
     </div>
   );
--- 09_useState_Hooks/src/propsTypes/PersonProps.ts
+++
@@ -1,7 +0,0 @@
-export type PersonProps = {
-  person: {
-    firstName: string;
-    lastName: string;
-  };
-  hobbies: string[];
-};
