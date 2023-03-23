442543b4022701a9417eab05193ad1d35a62ccaf
danielOuattara <daniel.ouattara@gmx.com>
2023-03-23 20:07:10 +0100

    03 logic update

--- 03_Typing_Props/src/_01_functional/components/Greet.tsx
+++ 03_Typing_Props/src/_01_functional/components/Greet.tsx
@@ -20,11 +20,11 @@ export const Greet = (props: GreetPropsType) => {
 
 //---------------------------------------------------------------------
 
-interface GreetPropsInterface {
+interface Greet2PropsInterface {
   name?: string | number;
   number?: number;
 }
-export const Greet2 = ({ name, number }: GreetPropsInterface) => {
+export const Greet2 = ({ name, number }: Greet2PropsInterface) => {
   return (
     <div>
       <h2>Welcome to React TypeScript, {name}</h2>
